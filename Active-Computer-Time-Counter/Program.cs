04e1c1bc770885eb204ea5abe4667675ef58edad
BuildTools <willebergh@outlook.com>
2019-11-10 14:20:20 +0100

    first commit

---
+++ Active-Computer-Time-Counter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Active_Computer_Time_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
